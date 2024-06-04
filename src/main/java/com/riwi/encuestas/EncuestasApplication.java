f3c72fc8cea885843609998b84cf711581bf9812
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-06-04 06:21:54 -0500

    projecto inicilializado

---
+++ src/main/java/com/riwi/encuestas/EncuestasApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.encuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuestasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/encuestas/EncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.encuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
